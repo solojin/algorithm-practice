4b6d2382db8717811a240b234241a5cb30be6a12
939249500@qq.com <939249500@qq.com>
2021-12-17 14:53:22 +0800

    sort

--- src/jinyj/course/sort/MergeSortReverse.java
+++ src/jinyj/course/sort/MergeSortReverse.java
@@ -2,43 +2,40 @@ package jinyj.course.sort;
 
 import jinyj.util.DataStore;
 
-// 归并排序
-public class MergeSort {
+// 归并法求逆序对
+public class MergeSortReverse {
 
     public static void main(String[] args) {
-        Integer[] arr = DataStore.getData();
+        // 逆序对：{2,1} {4,3}
+        Integer[] arr = {2,1,4,3};
         String source = "";
         for(int i = 0; i < arr.length; i++) {
             source += arr[i] + " ";
         }
         System.out.println("原数组：" + source);
-        MergeSort.sort(arr, 0, arr.length - 1);
+        Integer count = MergeSortReverse.process(arr, 0, arr.length - 1);
         String res = "";
         for(int i = 0; i < arr.length; i++) {
             res += arr[i] + " ";
         }
         System.out.println("排序后：" + res);
-    }
-
-    // 归并排序
-    public static void sort(Integer[] arr, int L, int R) {
-        MergeSort.sort(arr, L, R);
+        System.out.println("逆序对数：" + count);
     }
 
     // 二分后合并
-    public static void process(Integer[] arr, int L, int R) {
+    public static Integer process(Integer[] arr, int L, int R) {
         if(L == R) {
-            return;
+            return 0;
         }
         // 等价于L +（R - L）/ 2
         int mid = L + ((R - L) >> 1);
-        process(arr, L, mid);
-        process(arr, mid + 1, R);
-        merge(arr, L, mid, R);
+        return process(arr, L, mid) +
+                process(arr, mid + 1, R) +
+                merge(arr, L, mid, R);
     }
 
     // 合并
-    public static void merge(Integer[] arr, int L, int M, int R) {
+    public static Integer merge(Integer[] arr, int L, int M, int R) {
         // 辅助数组，用于存放R - L区间排序后的值
         Integer[] help = new Integer[R - L + 1];
         // 辅助数组当前位置
@@ -47,9 +44,19 @@ public class MergeSort {
         int p1 = L;
         // 右区间当前位置
         int p2 = M+1;
-        // arr[p1]和arr[p2]对比大小，小值放入辅助数组，并自身索引加1
+        // 逆序对数
+        int count = 0;
+        // arr[p1]和arr[p2]对比大小，大值放入辅助数组，并自身索引加1
         while(p1 <= M && p2 <= R) {
-            help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+            // 右区间已排好序，所以当arr[p1]比arr[p2]大时，右区间的数比arr[p1]小的个数是（R-p2+1）
+            if(arr[p1] > arr[p2]) {
+                count += (R - p2 + 1);
+                for(int k = p2; k <= R; k++) {
+                    System.out.println("逆序对：{" + arr[p1] + "," + arr[k] + "}");
+                }
+            }
+            // arr[p1]和arr[p2]对比大小，大值放入辅助数组，并自身索引加1
+            help[i++] = arr[p1] > arr[p2] ? arr[p1++] : arr[p2++];
         }
         // 补充，考虑p2已放完，p1未放完的情况
         while(p1 <= M) {
@@ -62,5 +69,6 @@ public class MergeSort {
         for(i = 0; i < help.length; i++) {
             arr[L + i] = help[i];
         }
+        return count;
     }
 }
--- src/jinyj/course/sort/MergeSortSmallSum.java
+++ src/jinyj/course/sort/MergeSortSmallSum.java
@@ -1,51 +1,62 @@
 package jinyj.course.sort;
 
-import jinyj.util.DataStore;
-//TODO 对照视频，未得到预期的小和值
+// 归并法求小和
 public class MergeSortSmallSum {
 
     public static void main(String[] args) {
-//        Integer[] arr = DataStore.getSmallData();
-//        Integer[] arr = {1,3,4,2,5};
-        Integer[] arr = {1,3,4,2};
+        // 小和值：2 + 1 + 2 + 1 = 6
+        Integer[] arr = {2,1,4,3};
+        String source = "";
         for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            source += arr[i] + " ";
         }
-        System.out.println("sort");
-        Integer res = MergeSortSmallSum.process(arr, 0, arr.length - 1);
+        System.out.println("原数组：" + source);
+        Integer smallSum = MergeSortSmallSum.process(arr, 0, arr.length - 1);
+        String res = "";
         for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            res += arr[i] + " ";
         }
-        System.out.println("smallSum");
-        System.out.println(res);
+        System.out.println("排序后：" + res);
+        System.out.println("小和：" + smallSum);
     }
 
+    // 二分后归并
     public static Integer process(Integer[] arr, int L, int R) {
         if(L == R) {
             return 0;
         }
+        // 等价于L +（R - L）/ 2
         int mid = L + ((R - L) >> 1);
-        process(arr, L, mid);
-        process(arr, mid + 1, R);
-        merge(arr, L, mid, R);
         return process(arr, L, mid)
                 + process(arr, mid + 1, R)
                 + merge(arr, L, mid, R);
     }
 
+    // 归并，并计算小和
     public static Integer merge(Integer[] arr, int L, int M, int R) {
+        // 辅助数组，用于存放R - L区间排序后的值
         Integer[] help = new Integer[R - L + 1];
+        // 辅助数组当前位置
         int i = 0;
+        // 左区间当前位置
         int p1 = L;
+        // 右区间当前位置
         int p2 = M+1;
         int res = 0;
         while(p1 <= M && p2 <= R) {
+            // 求小和等效于，把数组中每一个数自身累加，累加的次数为右边比它大的数的个数
+            // 右区间已排好序，所以当arr[p1]比arr[p2]小时，右区间的数比arr[p1]大的个数是（R-p2+1）
             res += arr[p1] < arr[p2] ? (R - p2 + 1) * arr[p1] : 0;
+            // arr[p1]和arr[p2]对比大小，小值放入辅助数组，并自身索引加1
+            // 与经典merge过程不一样的地方，这里arr[p1] == arr[p2]时，先拷贝arr[p2]进辅助数组，
+            // 因为这样才能保证arr[p1]比arr[p2]小时，（R-p2+1）一定是右区间的数比arr[p1]大的个数
             help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
         }
+        // 补充，考虑p2已放完，p1未放完的情况
         while(p1 <= M) {
             help[i++] = arr[p1++];
         }
+        // 补充，考虑p1已放完，p2未放完的情况
         while(p2 <= R) {
             help[i++] = arr[p2++];
         }
