9890b23e0ff43870202d87cc4a4d6f04e83b903e
939249500@qq.com <939249500@qq.com>
2021-12-20 15:33:48 +0800

    sort

---
+++ src/jinyj/course/sort/CountSort.java
@@ -0,0 +1,62 @@
+package jinyj.course.sort;
+
+import jinyj.util.DataStore;
+
+// 计数排序
+public class CountSort {
+
+    public static void main(String[] args) {
+        Integer[] arr = DataStore.getData();
+        String source = "";
+        for(int i = 0; i < arr.length; i++) {
+            source += arr[i] + " ";
+        }
+        System.out.println("原数组：" + source);
+        int[] sortArr = CountSort.sort(arr);
+        String res = "";
+        for(int i = 0; i < sortArr.length; i++) {
+            res += sortArr[i] + " ";
+        }
+        System.out.println("排序后：" + res);
+    }
+
+    // 计数排序
+    public static int[] sort(Integer[] arr) {
+        int max = CountSort.getMaxAndMin(arr);
+        return CountSort.count(arr, max);
+    }
+
+    // 获取数组中最大值
+    public static int getMaxAndMin(Integer[] arr) {
+        int max = 0;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] > max) {
+               max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    // 计数
+    public static int[] count(Integer[] arr, int max) {
+        // 计数数组，容量大小为原数组的最大值+1，每个位置上的值已被默认初始化为0
+        int[] countArr = new int[max + 1];
+        // 辅助数组，存放排序后的数组值
+        int[] helpArr = new int[arr.length];
+        // 遍历原数组，给计数数组对应位置数值+1
+        for(int i = 0; i < arr.length; i++) {
+            countArr[arr[i]] += 1;
+        }
+        // 对计数数组做累加和，累加和的结果，其实就是排序后的位置索引
+        for(int i = 1; i < countArr.length; i++) {
+            countArr[i] += countArr[i - 1];
+        }
+        // 遍历原数组，将累加和的结果作为新数组的位置索引，依次将值赋值到辅助数组
+        // 每取出一个值，就在计数数组对应位置-1
+        for(int i =  0; i < arr.length; i++) {
+            helpArr[countArr[arr[i]] - 1] = arr[i];
+            countArr[arr[i]]--;
+        }
+        return helpArr;
+    }
+}
--- src/jinyj/course/sort/HeapSort.java
+++ src/jinyj/course/sort/HeapSort.java
@@ -7,25 +7,42 @@ public class HeapSort {
 
     public static void main(String[] args) {
         Integer[] arr = DataStore.getData();
+        String source = "";
         for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            source += arr[i] + " ";
         }
-        System.out.println("sort");
+        System.out.println("原数组：" + source);
+        HeapSort.sort(arr);
+        String res = "";
+        for(int i = 0; i < arr.length; i++) {
+            res += arr[i] + " ";
+        }
+        System.out.println("排序后：" + res);
+    }
+
+    // 堆排
+    public static void sort(Integer[] arr) {
+        // 将数组转化为大根堆
         for(int i = 0; i < arr.length; i++) {
             HeapSort.headInsert(arr, i);
         }
+        // 堆容量
         int headSize = arr.length - 1;
         while(headSize > 0) {
+            // 取出最大值，即将堆的根节点与最后一个堆值交换，并让堆容量-1
             DataStore.swap(arr, 0, headSize);
-            HeapSort.headIfy(arr, 0, headSize--);
-        }
-        for(int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            headSize = headSize - 1;
+            // 调整堆顺序为大根堆
+            HeapSort.headIfy(arr, 0, headSize);
         }
     }
 
+    // 向堆中插入新值
     public static void headInsert(Integer[] arr, int i) {
         if(i >= 0) {
+            // 堆结构固定，arr[(i - 1) / 2]得到的是父节点
+            // 如果当前位置比父节点大，则与父节点交换值，并把当前位置i指向父节点
+            // 循环这一过程，直到当前位置的值不比父节点大，或者当前位置已到堆顶
             while(arr[i] > arr[(i - 1) / 2]) {
                 DataStore.swap(arr, i, (i - 1) / 2);
                 i = (i - 1) / 2;
@@ -33,17 +50,27 @@ public class HeapSort {
         }
     }
 
+    // 调整堆顺序为大根堆
     public static void headIfy(Integer[] arr, int i, int headSize) {
         if(i >= 0) {
+            // 堆结构固定，(2 * i + 1) 得到的是左子节点
+            // 左子节点小于等于堆容量时，即当前位置仍然有子节点时，继续循环
             while((2 * i + 1) <= headSize) {
+                // 左子节点
                 int l = 2 * i + 1;
+                // 堆结构固定，(2 * i + 2) 得到的是右子节点
                 int r = 2 * i + 2;
+                // 获取左右节点中的最大值
                 int large = r <= headSize && arr[r] > arr[l] ? r : l;
+                // 获取当前节点与左右子节点间的最大值
                 large = arr[large] > arr[i] ? large : i;
+                // 当最大值就是当前位置，说明已经是大根堆，跳出循环
                 if(large == i) {
                    return;
                 }
+                // 当前位置的值与最大值交换
                 DataStore.swap(arr, i, large);
+                // 当前位置指向最大值节点
                 i = large;
             }
         }
